cd95d70412c9401d7352c3a886e0a7774cea3064
Rémi Beaufils <remi.beaufils@fr.clara.net>
2019-07-09 10:48:03 +0200

    TER-407: add name_prefix variable for vnet name

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  default_name = "${var.client_name}-${var.location_short}-${var.environment}-vnet"
+  name_prefix = "${var.name_prefix != "" ? replace(var.name_prefix, "/[a-z0-9]$/", "$0-") : ""}"
+  default_name = "${local.name_prefix}${var.client_name}-${var.location_short}-${var.environment}-vnet"
 
   default_tags = {
     env   = "${var.environment}"
