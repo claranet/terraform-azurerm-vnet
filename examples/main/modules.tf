db94c5ab99f70f7e07e28c8597a229a2e29f27ac
Rémi Beaufils <remi.beaufils@fr.clara.net>
2021-09-20 16:55:03 +0200

    AZ-572: Revamp examples + improve CI

---
+++ examples/main/modules.tf
@@ -0,0 +1,32 @@
+module "azure_region" {
+  source  = "claranet/regions/azurerm"
+  version = "x.x.x"
+
+  azure_region = var.azure_region
+}
+
+module "rg" {
+  source  = "claranet/rg/azurerm"
+  version = "x.x.x"
+
+  location    = module.azure_region.location
+  client_name = var.client_name
+  environment = var.environment
+  stack       = var.stack
+}
+
+module "azure_virtual_network" {
+  source  = "claranet/vnet/azurerm"
+  version = "x.x.x"
+
+  environment    = var.environment
+  location       = module.azure_region.location
+  location_short = module.azure_region.location_short
+  client_name    = var.client_name
+  stack          = var.stack
+
+  resource_group_name = module.rg.resource_group_name
+
+  vnet_cidr   = ["10.10.0.0/16"]
+  dns_servers = ["10.0.0.4", "10.0.0.5"] # Can be empty if not used
+}
